c45e25ec5a238145e49c488b1d7454ff216803f2
Arthur <Arth.valadao@gmail.com>
2024-07-18 23:05:00 -0300

    insert nodemon and remove node modules in git

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ app.get('/', (request, response) => {
 });
 
 
-app.listen(3000,() => console.log('Server started at http://localhost:3000'))
+app.listen(3000, () => console.log('Server started at http://localhost:3000'))
